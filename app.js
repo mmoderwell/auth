6d2859e46fd33666b97607f12b361e7305897edb
mmoderwell <mmoderwell@gmail.com>
2018-05-30 20:43:16 -0500

    Work on login page

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
@@ -19,7 +23,15 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
 	mongo_uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/auth`;
 }
 
-mongoose.connect(mongo_uri).then(() => console.log('Connected to MongoDB.'));
+mongoose.connect(mongo_uri, { useMongoClient: true }).then(() => console.log('Connected to mongodb.'))
+	.catch((e) => {
+		console.error('Connection to mongodb failed.');
+	});
+
+//the database connection is disconnected
+mongoose.connection.on('disconnected', function() {
+	console.log('Connection to mongodb is disconnected.');
+});
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -33,50 +45,49 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use(session({
-	secret: 'blueberry pancakes',
-	cookie: {
-		maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-	},
-	store: new MongoDBStore({
-		uri: mongo_uri,
-		collection: 'sessions'
-	}),
-	resave: false,
-	saveUninitialized: false,
-	cookie: { secure: false }
-}));
+// app.use(session({
+// 	secret: 'blueberry pancakes',
+// 	cookie: {
+// 		maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+// 	},
+// 	store: new MongoDBStore({
+// 		uri: mongo_uri,
+// 		collection: 'sessions'
+// 	}),
+// 	resave: false,
+// 	saveUninitialized: false,
+// 	cookie: { secure: false }
+// }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-//not 100% what this does at the moment
-app.use((req, res, next) => {
-	res.locals.isAuthenticated = req.isAuthenticated();
-	next();
-});
+// //not 100% what this does at the moment
+// app.use((req, res, next) => {
+// 	res.locals.isAuthenticated = req.isAuthenticated();
+// 	next();
+// });
 
-passport.use(new LocalStrategy(
-	(username, password, done) => {
-		const User = require('./models/user');
-		const bcrypt = require('bcrypt');
+// passport.use(new LocalStrategy(
+// 	(username, password, done) => {
+// 		const User = require('./models/user');
+// 		const bcrypt = require('bcrypt');
 
-		User.findOne({ username: username }, function(err, user) {
-			if (err) return done(err);
-			if (!user) return done(null, false);
+// 		User.findOne({ username: username }, function(err, user) {
+// 			if (err) return done(err);
+// 			if (!user) return done(null, false);
 
-			let hashed = user.password;
-			bcrypt.compare(password, hashed, (err, response) => {
-				if (response === true) {
-					return done(null, user);
-				} else {
-					return done(null, false);
-				}
-			});
-		});
-	}
-));
-
+// 			let hashed = user.password;
+// 			bcrypt.compare(password, hashed, (err, response) => {
+// 				if (response === true) {
+// 					return done(null, user);
+// 				} else {
+// 					return done(null, false);
+// 				}
+// 			});
+// 		});
+// 	}
+// ));
 routes(app);
 
 // catch 404 and forward to error handler
